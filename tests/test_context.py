6cb5ca9c356551fa9046409aa39c43cf96c40926
brann <ogisajenajbolji@gmail.com>
2026-08-12 00:35:08 +0200

    add decorator_names test

--- tests/test_context.py
+++ tests/test_context.py
@@ -30,3 +30,16 @@ def test_dotted_name (expr, expected):
     node = ast.parse(expr, mode="eval").body
     assert dotted_name(node) == expected
     
+def test_decorator_names_handles_both_forms():
+    fun = parse_func(
+        '@app_commands.command(name="ban")\n'
+        'app_commands.describe(user="x")\n'
+        '@discord.ui.button\n'
+        'async def ban(): pass\n'
+    )
+    assert decorator_names(func) == [
+        # three strings in order
+        "app_commands.command",
+        "app_commands.describe",
+        "@discord.ui.button",
+    ]
